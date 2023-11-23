a72f28ac4b9a4bc3799522ac16cda7f3f9109d85
Alexander Disput <alexanderdisput@yahoo.com>
2023-11-23 14:18:07 +0000

    added basic CSS and JS structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-window.alert("hello")
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
